87ba72875657aa4200da446aca1a217c27734542
Priyam Upadhyay <priyamupadhyay17@msitprogram.net>
2022-04-18 23:54:22 +0530

    implemented create chart

--- hw6_protein.py
+++ hw6_protein.py
@@ -288,6 +288,12 @@ Returns: None
 '''
 def createChart(xLabels, freqList1, label1, freqList2, label2, edgeList=None):
     import matplotlib.pyplot as plt
+    w = 0.35
+    plt.bar(xLabels, freqList1, width = -w, align="edge", label = label1, edgecolor = edgeList)
+    plt.bar(xLabels, freqList2, width = w, align="edge", label = label2, edgecolor = edgeList)
+    plt.xticks(rotation = "vertical")
+    plt.legend()
+    plt.show()
     return
 
 
